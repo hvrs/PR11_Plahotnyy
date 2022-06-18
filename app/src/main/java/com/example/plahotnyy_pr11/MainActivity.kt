993b7f108b1c8cecdc32c9767e08cf033e6c6457
HvRs <hvrsttk@gmail.com>
2022-06-18 15:04:30 +0700

    123

---
+++ app/src/main/java/com/example/plahotnyy_pr11/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.plahotnyy_pr11
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onCheckboxClicked(view: View) {
+        // Получаем Функция перехода на другой экран:
+        val checkBox = view as CheckBox
+        // Получаем, отмечен ли данный флажок
+        if (checkBox.isChecked) {
+        }
+    }
+    fun onClickStepsActivity(view: View){
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+
+
+
+}
